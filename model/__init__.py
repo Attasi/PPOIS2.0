16c275b1e61de387085b20bc81c48e3dbc24d464
Attasi <nastya28092006nastya@gmail.com>
2026-05-21 20:52:42 +0300

    data_manager change

--- model/__init__.py
+++ model/__init__.py
@@ -2,5 +2,5 @@
 Модуль model содержит классы данных для приложения.
 """
 
-from model.student_record import StudentRecord, create_sample_record
-from model.data_manager import DataManager, create_test_data_file
+from model.student_record import StudentRecord
+from model.data_manager import DataManager
